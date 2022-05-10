5b741c114e715980b55bf9e682d1182c7c430540
Paulo5812 <paulo5812@live.se>
2022-05-10 21:23:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// List of cities we need to join.
+List<string> users = new List<string>();
+users.Add("Paulo");
+users.Add("Chico");
+users.Add("Pele");
+users.Add("Marcus");
+
+    // Join strings into one CSV line.
+string line = string.Join(",", users.ToArray());
+Console.WriteLine(line);
+   
